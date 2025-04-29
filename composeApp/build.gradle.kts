c9c5c59226fb903b0bcce95d12d7c433e1663111
Alexey Zarubin <alken1t15@gmail.com>
2025-04-29 14:56:20 +0300

    add ui

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    jvm("desktop")
+
+    sourceSets {
+        val desktopMain by getting
+
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+        }
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutines.swing)
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "org.spb.project.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.spb.project"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ composeApp/src/desktopMain/kotlin/org/spb/project/model/CircleNode.kt
@@ -0,0 +1,6 @@
+import androidx.compose.ui.geometry.Offset
+
+data class CircleNode(
+    var offset: Offset,
+    val radius: Float = 20f
+)
